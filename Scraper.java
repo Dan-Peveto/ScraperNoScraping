23c951208881488e30b7616fc2e651b204ba2784
dan-peveto <daniel_peveto@outlook.com>
2023-12-06 13:14:28 -0800

    Added Scraper.java

---
+++ Scraper.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Scraper extends JFrame {
+    public Scraper() {
+        super("Scraper No Scraping");
+        setSize(800, 800);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
